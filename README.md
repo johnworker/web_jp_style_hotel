098c7f4aa66e21de444359d8c26dbe318bd2f22f
johnworker <besscomdark@gmail.com>
2024-06-26 15:08:10 +0800

    調整主頁完成

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # web_jp_style_hotel
 日式旅館品牌網站
+
+|-- index.html 
+|-- style.css 
+|-- style.css.map 
+|-- style.scss 
+|-- main.js 
+|-- /images 
+    |-- font_logo.png 
+    |-- show_hotel.mp4
--- main.js
+++ main.js
@@ -2,4 +2,15 @@
 $(".menu_btn").click(function () {
     $(".menu_wrap").toggleClass("active");
     $(this).toggleClass("active");
-  });
+});
+
+
+// 滾動效果
+$(window).scroll(function () {
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop > 0) {
+        $('.header_load').css('background', 'rgba(0, 0, 0, 0)');
+    } else {
+        $('.header_load').css('background', '#ffffff');
+    }
+});
